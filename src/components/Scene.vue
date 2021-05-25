90507c80590d6d255b86d1078fd7edf726d63fd4
urukov <urukov@keysystems.ru>
2021-05-26 00:29:19 +0300

    обработка автомобилей и их позиций

--- src/components/Scene.vue
+++ src/components/Scene.vue
@@ -18,6 +18,7 @@ export default {
   },
   data() {
     return {
+      wsReceivedMessages: 0,
       added: false,
       mutex: false,
       is_logged: false,
@@ -53,35 +54,25 @@ export default {
       console.log(message)
     },
     wsOnMessage(message) {
-      const data = JSON.parse(message.data)
-      console.log(data)
-      this.ws_cars = data
-      if (!this.is_logged) {
-        console.log(data)
-        this.is_logged = true
-      }
+      this.ws_cars = JSON.parse(message.data)
+      this.wsReceivedMessages += 1
+      this.websocket.send(true)
     },
     initialize: function () {
       this.clock = new THREE.Clock()
       this.container = document.getElementById('container')
 
       this.renderer = new THREE.WebGLRenderer()
-      // this.renderer.shadowMap.enabled = true
-      // this.renderer.shadowMap.type = THREE.BasicShadowMap
       this.renderer.setSize(this.container.clientWidth, this.container.clientHeight)
       this.container.appendChild(this.renderer.domElement)
 
       this.scene = new THREE.Scene()
-      // this.ambientLight = new THREE.AmbientLight(0xffffff, 0.05)
 
       this.light = new THREE.PointLight(0xffffff, 0.8, 100)
       this.light.position.set(5,5,5)
       this.light.castShadow = true
       this.light.shadow.camera.near = 0.1
       this.light.shadow.camera.far = 100
-
-      // this.scene.add(this.ambientLight)
-      // this.scene.add(this.light)
 
       this.fpsCameraControl = new FpsCameraControl(
           new THREE.PerspectiveCamera(
@@ -93,13 +84,10 @@ export default {
       const material = new THREE.MeshBasicMaterial({ color: 0x0000ff })
       const geometry = new THREE.BoxGeometry(1,1,1)
       this.cube = new THREE.Mesh(geometry, material)
-      // this.cube.receiveShadow = true
-      // this.cube.castShadow = true
 
       const panel_material = new THREE.MeshBasicMaterial({color: 0x00ff00})
       const panel_geometry = new THREE.PlaneGeometry(1000, 1000)
       this.panel = new THREE.Mesh(panel_geometry, panel_material)
-      // this.panel.receiveShadow = true
       this.panel.position.set(0,0,0)
       this.panel.lookAt(0,1,0)
 
@@ -116,28 +104,27 @@ export default {
     },
     simulate: function () {
       this.fpsCameraControl.simulate(this.clock.getDelta())
+      // обработка положений автомобилей
+      const material = new THREE.MeshBasicMaterial({ color: 0x0000ff })
+      const geometry = new THREE.BoxGeometry(1,1,1)
+      if (this.ws_cars !== null) {
+        for (let i = 0; i < this.ws_cars.length; i++) {
+          let car = this.ws_cars[i]
+          const scene_car = this.scene.getObjectByName(car.id)
+          let position = car.position
+          if (!scene_car) {
+            let cube = new THREE.Mesh(geometry, material)
+            cube.name = car.id
+            cube.position.set(position.x, 0.1, position.y)
+            this.scene.add(cube)
+          } else {
+            scene_car.position.set(position.x, 0.1, position.y)
+          }
+        }
+      }
       this.draw()
     },
     draw: function () {
-      const material = new THREE.MeshBasicMaterial({ color: 0x0000ff })
-      const geometry = new THREE.BoxGeometry(1,1,1)
-      if (this.ws_cars !== null && !this.added) {
-        for (let i = 0; i < this.ws_cars.length; i++) {
-          let car = this.ws_cars[i]
-          const scene_car = this.scene.getObjectById(car.id)
-          if (!scene_car) {
-            let cube = new THREE.Mesh(geometry, material)
-            let position = car.position
-            cube.position.set(position.x, 0.1, position.y)
-            console.log(cube.id)
-            this.scene.add(cube)
-            this.scene.remove()
-            console.log('added')
-            this.added = true
-          }
-        }
-
-      }
       requestAnimationFrame(this.simulate)
       this.renderer.render(this.scene, this.fpsCameraControl.camera)
     },
